039907679f241ec7073c53f79efd35d192c6e7ee
jone0315 <takz159@gmail.com>
2017-04-03 15:58:20 +0800

    修改Photo遊戲 改為記憶遊戲

--- my_game/src/main/java/com/example/my_game/photo/Photo.java
+++ my_game/src/main/java/com/example/my_game/photo/Photo.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +19,8 @@ import android.widget.CursorAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.my_game.R;
@@ -31,10 +33,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import static com.example.my_game.R.drawable.t03;
+
 public class Photo extends AppCompatActivity {
     private Context context;
     private List<Map<String, Object>> list;
-    private SimpleAdapter adapter;
+    private PhotoAdapter adapter;
     private SimpleCursorAdapter adapter1;
     private int nextInt = 0, id1 = 100;
     private String data;
@@ -45,10 +49,15 @@ public class Photo extends AppCompatActivity {
     private int num = 3;
     private MediaPlayer mp;
     private UserModel model;
-    final int[] ar = {R.drawable.cat, R.drawable.dog, R.drawable.images, R.drawable.t01, R.drawable.t02, R.drawable.t03
+    final int[] ar = {R.drawable.cat, R.drawable.dog, R.drawable.images, R.drawable.t01, R.drawable.t02, t03
             , R.drawable.t04, R.drawable.t05, R.drawable.t06, R.drawable.t07}; //圖片
     final int[] ar1 = {R.drawable.b01,R.drawable.b02,R.drawable.b03,R.drawable.b04,R.drawable.b05,R.drawable.b06,R.drawable.b07,R.drawable.b08,R.drawable.b09,R.drawable.b10};
     final String[] str = {"cat", "dog", "t08", "t01", "t02", "t03", "t04", "t05", "t06", "t07"}; //名稱 再點的時候可以比對
+    private Handler handler;
+    private int count = 0;
+    private TextView textView;
+    public CountDownTimer ctime;
+    private Map<String,Object> map2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,65 +67,133 @@ public class Photo extends AppCompatActivity {
         setTitle("找相同圖");
         chronometer = (Chronometer) findViewById(R.id.chronometer2);
         list = new ArrayList<>();
-
+        handler = new Handler();
         gv01 = (GridView) findViewById(R.id.gv01);
+        textView = (TextView) findViewById(R.id.textView20);
+        ctime = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                count = -1;
+                textView.setVisibility(View.VISIBLE);
+                textView.setText(millisUntilFinished/1000+"");
+            }
+
+            @Override
+            public void onFinish() {
+                count =0;
+                textView.setText(0+"");
+                textView.setVisibility(View.GONE);
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+            }
+        };
+
+
 
         PhotoDB gameDB = new PhotoDB(context,"photo.db",null,1);
         db = gameDB.getWritableDatabase();
+        play();
+
+        gv01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Map<String, Object> map = list.get(position);
+
+                String data2 = map.get("str").toString();
+                String data3 = map.get("str").toString();
+
+
+                if (count !=-1 &&!data2.equals("123")&&!data3.equals("123")) {
+                    count++;
+                    handler = new Handler();
+
+
+                    int photo = (Integer) map.get("image");
+                    t01 = (ImageView) view;
+                    t01.setImageResource(photo);
+
+
+                    if (data2.equals(data) && id != id1 && !data2.equals("123")) {
+                        nextInt++;
+                        map.put("str","123");
+                        if(map2 !=null){
+                            map2.put("str","123");
+                        }
+
+                        t01.setImageResource(photo);
+                        t02.setImageResource((Integer) map2.get("image"));
+                        mp =  MediaPlayer.create(context,R.raw.striking);
+                        count = 0;
+                        mp.start();
+                        if (nextInt == 10) {
+                            String temp = chronometer.getText().toString();
+                            chronometer.stop();
+                            Toast.makeText(context, temp + "恭喜完成遊戲", Toast.LENGTH_LONG).show();
+                            store(temp);
+
+                        }
+                    }
+
+                    data = map.get("str").toString();
+                    id1 = (int) id;
+
+                    if(count == 2 ){
+
+                        seephoto(t01,t02);
+                        t02 =null;
+                        data = "";
+                        id1 = -1;
+                        count = 0;
+                    }
+                    t02 = (ImageView) view;
+                    map2 = map;
+                }
+
+            }
+        });
+    }
+    public void seephoto(final ImageView t01,final ImageView t02){
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                t01.setImageResource(R.drawable.ipod);
+                t02.setImageResource(R.drawable.ipod);
+
+            }
+        },1000);
+    }
+
+
+    public void play(){
+        ctime.start();
+        list.clear();
+        num = new Random().nextInt(10)+1;
         for (int i = 0; i < ar1.length; i++) {
             Map<String, Object> imet = new HashMap<String, Object>();
-            imet.put("image",ar1[i] );
-            imet.put("image2",ar[i] );
+            if(num%2 == 0) {
+                imet.put("image", ar1[i]);
+            }else {
+                imet.put("image", ar[i]);
+            }
             imet.put("str", str[i]);
             list.add(imet);
             list.add(imet);
         }//將資料轉成Map放入list
-//
 
 
 
-
-        play();
-        gv01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Map<String, Object> map = list.get(position);
-                String data2 = map.get("str").toString();
-                t01 = (ImageView) view;
-                if (data2.equals(data) && id != id1) {
-                    nextInt++;
-                    t01.setImageResource(R.drawable.ok);
-                    t02.setImageResource(R.drawable.ok);
-                    mp =  MediaPlayer.create(context,R.raw.striking);
-                    mp.start();
-                    if (nextInt == 10) {
-                        String temp = chronometer.getText().toString();
-                        chronometer.stop();
-                        Toast.makeText(context, temp + "恭喜完成遊戲", Toast.LENGTH_LONG).show();
-                        store(temp);
-
-                    }
-                }
-                t02 = (ImageView) view;
-                data = map.get("str").toString();
-                id1 = (int) id;
-            }
-        });
-
-    }
-
-    public void play(){
-        num = new Random().nextInt(10)+1;
         //context, 要放的東西 , 放東西的容器 , 要放的東西名子  , 要放的容器 名子順序與容器照排會對應到相對得位子
-        adapter = new SimpleAdapter(context, list, R.layout.imrow, new String[]{num>=5?"image":"image2"}, new int[]{R.id.imageView});
-//        adapter.notifyDataSetChanged();
+        adapter = new PhotoAdapter(list,context);
+//        adapter = new SimpleAdapter(context, list, R.layout.imrow, new String[]{num>=5?"image":"image2"}, new int[]{R.id.imageView});
+////        adapter.notifyDataSetChanged();
         gv01.setAdapter(adapter);
         Collections.shuffle(list);//洗牌
         adapter.notifyDataSetChanged();//更新排序畫面
+        chronometer.stop();
+        ctime.cancel();
+        ctime.start();
 
-
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
         nextInt = 0;
         data = "";
 
@@ -165,9 +242,7 @@ public class Photo extends AppCompatActivity {
         listView.setAdapter(adapter1);
         builder.setNegativeButton("取消",null);
         builder.show();
-
-
-
-
     }
+
+
 }
